e49e1e0303cda18c42850a6ddf870eef62a9ca6d
Bomber57 <[adenewellis@gmail.com]>
2022-04-14 16:56:40 -0400

    Baisc implementations of all routes on time calc page

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 original_file = pd.read_csv("merged_stop_data.csv", dtype = str)
 calc = TimeCalculator()
 
-gold_route_internal = { "lightrail": "Wallis Hall E/Light Rail",
+routes_internal = { "lightrail": "Wallis Hall E/Light Rail",
                         "studenthealth": "Student Health E",
                         "fretwellS": "Fretwell South",
                         "catoS": "Cato Hall S",
@@ -22,7 +22,17 @@ gold_route_internal = { "lightrail": "Wallis Hall E/Light Rail",
                         "fretwellN": "Fretwell N",
                         "science": "Aux Services West", ##Science Building
                         "studentu": "Student Union W",
-                        "belk": "Union Deck/Belk N"
+                        "belk": "Union Deck/Belk N",
+                        "lightrailW": "Wallis Hall W/Light Rail",
+                        "belkS": "Belk Hall S",
+                        "unionE": "Student Union E",
+                        "auxE": "Aux Services East",
+                        "fretwellS": "Fretwell S",
+                        "catoS": "Cato Hall S",
+                        "robinsonS": "Robinson Hall S",
+                        "reeseW": "Reese West",
+                        "coneW": "Cone Deck W",
+                        "southVillage": "South Village Deck"
                         }
 
 ### Site Pages ###
@@ -43,13 +53,22 @@ def time_calculator():
 def calc_result():
    firstStop = request.form['Stop1']
    secondStop = request.form['Stop2']
-   firstStop = gold_route_internal[firstStop]
-   secondStop = gold_route_internal[secondStop]
+   firstStop = routes_internal[firstStop]
+   secondStop = routes_internal[secondStop]
    now = datetime.now()
    median = calc.calculate_distance(original_file, firstStop, secondStop, now.hour, now.weekday(), now.month)
+   firstStop = dict_key_lookup(routes_internal, firstStop)
+   secondStop = dict_key_lookup(routes_internal, secondStop)
    return render_template('Time Calculator.html', result = median, stop1 = firstStop, stop2 = secondStop, calcSuccess = True)
 
-
+### Helper Functions ###
+def dict_key_lookup(dict, val):
+   goalKey = ""
+   for key in dict.keys():
+      if dict[key] == val:
+         goalKey = dict[key]
+         break
+   return goalKey
 
 if __name__ == '__main__':
    app.debug = True
