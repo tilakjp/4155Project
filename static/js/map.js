f209bebe57760146c3e56d6200911e0e3aec6781
Arsh Patel <78064369+apate227@users.noreply.github.com>
2022-04-25 12:21:56 -0400

    Changes in Stops and trying Building outlines

--- static/js/map.js
+++ static/js/map.js
@@ -1,212 +1,490 @@
- const apiKey = 'pk.eyJ1IjoiYXBhdGUyMjciLCJhIjoiY2wyOTNvemxxMDM4cTNkcnV6OTJ2YWU4ZSJ9.2Md9glTJJSkravRFWvtH0Q';
+const apiKey = 'pk.eyJ1IjoiYXBhdGUyMjciLCJhIjoiY2wyOTNvemxxMDM4cTNkcnV6OTJ2YWU4ZSJ9.2Md9glTJJSkravRFWvtH0Q';
 
- const   map = L.map('map').setView([35.306872, -80.734292 ], 15);
+const   map = L.map('map').setView([35.306872, -80.734292 ], 15);
 
- L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-     maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: apiKey
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 18,
+   id: 'mapbox/streets-v11',
+   tileSize: 512,
+   zoomOffset: -1,
+   accessToken: apiKey
 }).addTo(map); 
 
 //Union West marker
+let unionWestTmp = `
 
+<h3>Union West</h3>
+<h4> Silver and Gold </h4>
+
+`
 const unionWest = L.marker([35.308189, -80.732685]).addTo(map);
-unionWest.bindPopup('<h1>Union West</h1>');
+unionWest.bindPopup(unionWestTmp);
 
 //Union East marker
+let unionEastTmp = `
 
+<h3>Union East</h3>
+<h4> Silver and Green </h4>
+
+`
 const unionEast = L.marker([35.308034, -80.73285]).addTo(map);
-unionEast.bindPopup('<h1>Union East</h1>');
+unionEast.bindPopup(unionEastTmp);
 
 //Auxiliary services marker
+let auxiliaryServicesTmp = `
 
+<h3>Auxiliary Services</h3>
+<h4> Silver and Green </h4>
+
+`
 const auxiliaryServices = L.marker([35.308032, -80.730564]).addTo(map);
-auxiliaryServices.bindPopup('<h1>Auxiliary Services</h1>');
+auxiliaryServices.bindPopup(auxiliaryServicesTmp);
 
 //Fretwell North marker
+let fretwellNorthTmp = `
 
+<h3>Fretwell North</h3>
+<h4> Silver, Green and Gold </h4>
+
+`
 const fretwellNorth = L.marker([35.307377, -80.729290]).addTo(map);
-fretwellNorth.bindPopup('<h1>Fretwell North</h1>');
+fretwellNorth.bindPopup(fretwellNorthTmp);
 
 //Fretwell South marker
+let fretwellSouthTmp = `
 
+<h3>Fretwell South</h3>
+<h4> Green and Gold </h4>
+
+`
 const fretwellSouth = L.marker([35.306983, -80.729515]).addTo(map);
-fretwellSouth.bindPopup('<h1>Fretwell South</h1>');
+fretwellSouth.bindPopup(fretwellSouthTmp);
 
 //Cato Hall South marker
+let catoHSouthTmp = `
 
+<h3>Cato Hall South</h3>
+<h4> Green and Gold </h4>
+
+`
 const catoHSouth = L.marker([35.304976, -80.728401]).addTo(map);
-catoHSouth.bindPopup('<h1>Cato Hall South</h1>');
+catoHSouth.bindPopup(catoHSouthTmp);
 
 //Cato Hall North marker
+let catoHNorthTmp = `
 
+<h3>Cato Hall North</h3>
+<h4> Green and Gold </h4>
+
+`
 const catoHNorth = L.marker([35.304993, -80.728107]).addTo(map);
-catoHNorth.bindPopup('<h1>Cato Hall North</h1>');
+catoHNorth.bindPopup(catoHNorthTmp);
 
 //Science building marker
+let scienceBuildingTmp = `
 
+<h3>Science Building</h3>
+<h4> Silver and Gold </h4>
+
+`
 const scienceBuilding = L.marker([35.308144, -80.730247]).addTo(map);
-scienceBuilding.bindPopup('<h1>Science Building</h1>');
+scienceBuilding.bindPopup(scienceBuildingTmp);
 
 //Robinson Hall South marker
+let robinsonHSouthTmp = `
 
+<h3>Robinson Hall South</h3>
+<h4> Green and Gold </h4>
+
+`
 const robinsonHSouth = L.marker([35.303587, -80.729415]).addTo(map);
-robinsonHSouth.bindPopup('<h1>Robinson Hall South</h1>');
+robinsonHSouth.bindPopup(robinsonHSouth);
 
 //Robinson Hall North marker
+let robinsonHNorthTmp = `
 
+<h3>Robinson Hall North</h3>
+<h4> Green and Gold </h4>
+
+`
 const robinsonHNorth = L.marker([35.303331, -80.729402]).addTo(map);
-robinsonHNorth.bindPopup('<h1>Robinson Hall North</h1>');
+robinsonHNorth.bindPopup(robinsonHNorthTmp);
 
 //Levine Hall West marker
+let levineHWestTmp = `
 
+<h3>Levine Hall West</h3>
+<h4> Gold </h4>
+
+`
 const levineHWest = L.marker([35.302062, -80.733021]).addTo(map);
-levineHWest.bindPopup('<h1>Levine Hall West</h1>');
-
-//Levine Hall East marker
-
-const levineHEast = L.marker([35.302017, -80.732758]).addTo(map);
-levineHEast.bindPopup('<h1>Levine Hall East</h1>');
+levineHWest.bindPopup(levineHWestTmp);
 
 //Hunt Hall marker
+let huntHallTmp = `
 
+<h3>Hunt Hall</h3>
+<h4> Gold </h4>
+
+`
 const huntHall = L.marker([35.301260, -80.735547]).addTo(map);
-huntHall.bindPopup('<h1>Hunt Hall</h1>');
+huntHall.bindPopup(huntHallTmp);
 
 //South Village Deck marker
+let soViDeckTmp = `
 
+<h3>South Village Deck</h3>
+<h4> Green </h4>
+
+`
 const soViDeck = L.marker([35.301067, -80.735815]).addTo(map);
-soViDeck.bindPopup('<h1>South Village Deck</h1>');
+soViDeck.bindPopup(soViDeckTmp);
 
 //Alumni Way East marker
+let alumniWEastTmp = `
 
+<h3>Alumni Way East</h3>
+<h4> Green </h4>
+
+`
 const alumniWEast = L.marker([35.302522, -80.737626]).addTo(map);
-alumniWEast.bindPopup('<h1>Alumni Way East</h1>');
+alumniWEast.bindPopup('<h3>Alumni Way East</h3>');
 
 //Alumni Way West marker
+let alumniWWestTmp = `
 
+<h3>Alumni Way West</h3>
+<h4> Gold </h4>
+
+`
 const alumniWWest = L.marker([35.302938, -80.737700]).addTo(map);
-alumniWWest.bindPopup('<h1>Alumni Way West</h1>');
+alumniWWest.bindPopup(alumniWWestTmp);
 
-//Resse West marker
+//Reese West marker
+let reeseWestTmp = `
 
+<h3>Reese West</h3>
+<h4> Green </h4>
+
+`
 const reeseWest = L.marker([35.304483, -80.732801]).addTo(map);
-reeseWest.bindPopup('<h1>Reese West</h1>');
+reeseWest.bindPopup(reeseWestTmp);
 
-//Resse East marker
+//Reese East marker
+let reeseEastTmp = `
 
+<h3>Reese East</h3>
+<h4> Gold </h4>
+
+`
 const reeseEast = L.marker([35.304287, -80.732822]).addTo(map);
-reeseEast.bindPopup('<h1>Reese East</h1>');
+reeseEast.bindPopup(reeseEastTmp);
 
 //Cone Deck marker
+let coneDeckTmp = `
 
+<h3>Cone Deck</h3>
+<h4> Green </h4>
+
+`
 const coneDeck = L.marker([35.304281, -80.734590]).addTo(map);
-coneDeck.bindPopup('<h1>Cone Deck</h1>');
+coneDeck.bindPopup(coneDeckTmp);
 
-//Student Union marker
+//Student Union Deck marker
+let studentUnionDeckTmp = `
 
-const studentUnion = L.marker([35.309157, -80.736394]).addTo(map);
-studentUnion.bindPopup('<h1>Student Union</h1>');
+<h3>Student Union Deck</h3>
+<h4> Gold </h4>
+
+`
+const studentUnionDeck = L.marker([35.309157, -80.736394]).addTo(map);
+studentUnionDeck.bindPopup(studentUnionDeckTmp);
 
 //Belk Hall South marker
+let belkHSouthTmp = `
 
+<h3>Belk Hall South</h3>
+<h4> Green </h4>
+
+`
 const belkHSouth = L.marker([35.310363, -80.736182]).addTo(map);
-belkHSouth.bindPopup('<h1>Belk Hall South</h1>');
+belkHSouth.bindPopup(belkHSouthTmp);
 
 //Light Rail West marker
+let lightRailWTmp = `
 
+<h3>Light Rail West</h3>
+<h4> Green </h4>
+
+`
 const lightRailW = L.marker([35.311955, -80.733446]).addTo(map);
-lightRailW.bindPopup('<h1>Light Rail West</h1>');
+lightRailW.bindPopup(lightRailWTmp);
 
 //Light Rail East marker
+let lightRailETmp = `
 
+<h3>Light Rail East</h3>
+<h4> Gold </h4>
+
+`
 const lightRailE = L.marker([35.311754, -80.733180]).addTo(map);
-lightRailE.bindPopup('<h1>Light Rail East</h1>');
+lightRailE.bindPopup(lightRailETmp);
 
 //North Deck marker
+let northDeckTmp = `
 
+<h3>North Deck</h3>
+<h4> Green </h4>
+
+`
 const northDeck = L.marker([35.313448, -80.731930]).addTo(map);
-northDeck.bindPopup('<h1>North Deck</h1>');
+northDeck.bindPopup(northDeckTmp);
 
 // FM/PPS marker
+let fmpTmp = `
 
+<h3>FM/PPS</h3>
+<h4> Green </h4>
+
+`
 const fmp = L.marker([35.311668, -80.730329]).addTo(map);
-fmp.bindPopup('<h1>FM/PPS</h1>');
+fmp.bindPopup(fmpTmp);
 
 // Student Health East marker
+let studentHealthETmp = `
 
+<h3>Student Health East</h3>
+<h4> Gold </h4>
+
+`
 const studentHealthE = L.marker([35.311549, -80.730625]).addTo(map);
-studentHealthE.bindPopup('<h1>Student Health East</h1>');
+studentHealthE.bindPopup(studentHealthETmp);
 
 // Student Health North marker
+let studentHealthNTmp = `
 
+<h3>Student Health North</h3>
+<h4> Silver and Green </h4>
+
+`
 const studentHealthN = L.marker([35.310579, -80.729380]).addTo(map);
-studentHealthN.bindPopup('<h1>Student Health North</h1>');
+studentHealthN.bindPopup(studentHealthNTmp);
 
 // Martin Hall marker
+let martinHallTmp = `
 
+<h3>Hunt Hall</h3>
+<h4> Silver </h4>
+
+`
 const martinHall = L.marker([35.310325, -80.726784]).addTo(map);
-martinHall.bindPopup('<h1>Martin Hall</h1>');
+martinHall.bindPopup(martinHallTmp);
 
 // Lot 6 marker
+let lotSixTmp = `
 
+<h3>Lot 6</h3>
+<h4> Silver </h4>
+
+`
 const lotSix = L.marker([35.308852, -80.725144]).addTo(map);
-lotSix.bindPopup('<h1>Lot 6</h1>');
+lotSix.bindPopup(lotSixTmp);
 
 // Lot 5A marker
+let lotFiveATmp = `
 
+<h3>Lot 5A</h3>
+<h4> Silver </h4>
+
+`
 const lotFiveA = L.marker([35.307241, -80.725246]).addTo(map);
-lotFiveA.bindPopup('<h1>Lot 5A</h1>');
+lotFiveA.bindPopup(lotFiveATmp);
 
 // East Deck 2 marker
+let eastDeckTwoTmp = `
 
+<h3>East Deck Two</h3>
+<h4> Silver </h4>
+
+`
 const eastDeckTwo = L.marker([35.306392, -80.726783]).addTo(map);
-eastDeckTwo.bindPopup('<h1>East Deck 2</h1>');
+eastDeckTwo.bindPopup(eastDeckTwoTmp);
 
 // Athletics Complex West marker
+let athleticsCompWTmp = `
 
+<h3>Hunt Hall</h3>
+<h4> Gold </h4>
+
+`
 const athleticsCompW = L.marker([35.307501, -80.739756]).addTo(map);
-athleticsCompW.bindPopup('<h1>Athletics Complex West</h1>');
+athleticsCompW.bindPopup(athleticsCompWTmp);
 
 // Athletics Complex East marker
+let athleticsCompETmp = `
 
+<h3>Athletics Complex East</h3>
+<h4> Silver </h4>
+
+`
 const athleticsCompE = L.marker([35.307383, -80.739870]).addTo(map);
-athleticsCompE.bindPopup('<h1>Athletics Complex East</h1>');
+athleticsCompE.bindPopup(athleticsCompETmp);
 
 // EPIC North marker
+let epicNorthTmp = `
 
+<h3>EPIC North</h3>
+<h4> Silver </h4>
+
+`
 const epicNorth = L.marker([35.309370, -80.741294]).addTo(map);
-epicNorth.bindPopup('<h1>EPIC North</h1>');
+epicNorth.bindPopup(epicNorthTmp);
 
 // EPIC South marker
+let epicSouthTmp = `
 
+<h3>EPIC South</h3>
+<h4> Silver </h4>
+
+`
 const epicSouth = L.marker([35.310059, -80.741933]).addTo(map);
-epicSouth.bindPopup('<h1>EPIC South</h1>');
+epicSouth.bindPopup(epicSouthTmp);
 
 // Grigg Hall marker
+let griggHallTmp = `
 
+<h3>Grigg Hall</h3>
+<h4> Silver </h4>
+
+`
 const griggHall = L.marker([35.310887, -80.741418]).addTo(map);
-griggHall.bindPopup('<h1>Grigg Hall</h1>');
+griggHall.bindPopup(griggHallTmp);
 
 // Motorsports marker
+let motorsportsTmp = `
 
+<h3>Motorsports</h3>
+<h4> Silver </h4>
+
+`
 const motorsports = L.marker([35.312843, -80.741028]).addTo(map);
-motorsports.bindPopup('<h1>Motorsports</h1>');
+motorsports.bindPopup(motorsportsTmp);
 
 // Duke Centennial Hall marker
+let dukeCentennialHallTmp = `
 
+<h3>Duke Centennial Hall</h3>
+<h4> Silver </h4>
+
+`
 const dukeCentennialHall = L.marker([35.312252, -80.741665]).addTo(map);
-dukeCentennialHall.bindPopup('<h1>Duke Centennial Hall</h1>');
+dukeCentennialHall.bindPopup(dukeCentennialHallTmp);
 
 // Portal West marker
+let portalWestTmp = `
 
+<h3>Portal West</h3>
+<h4> Silver </h4>
+
+`
 const portalWest = L.marker([35.311106, -80.743099]).addTo(map);
-portalWest.bindPopup('<h1>Portal West</h1>');
+portalWest.bindPopup(portalWestTmp);
 
 // CRI Deck marker
+let criDeckTmp = `
 
+<h3>CRI Deck</h3>
+<h4> Silver </h4>
+
+`
 const criDeck = L.marker([35.309385, -80.744113]).addTo(map);
-criDeck.bindPopup('<h1>CRI Deck</h1>');
+criDeck.bindPopup(criDeckTmp);
 
+//BUILDINGS OUTLINE
+
+// Student Union
+
+const studentUnion = L.polygon([
+   [35.308226, -80.733409],
+   [35.308473, -80.733095],
+   [35.308987, -80.733327],
+   [35.308994, -80.734043],
+   [35.308757, -80.734384],
+   [35.308521, -80.734375],
+   [35.308227, -80.734079]
+],{
+   color: 'blue',
+   fillColor: 'blue',
+   fillOpacity: 0.2,
+
+}).addTo(map)  
+
+//Cato College of Education
+
+const catoCollege = L.polygon([
+   [35.307783, -80.734438],
+   [35.307382, -80.734422],
+   [35.307283, -80.734226],
+   [35.307264, -80.733923],
+   [35.307364, -80.733829],
+   [35.307772, -80.733819],
+   [35.307903, -80.733913],
+   [35.307899, -80.734224]
+],{
+   color: 'blue',
+   fillColor: 'blue',
+   fillOpacity: 0.2,
+
+}).addTo(map)  
+
+//College of Health and Human Sciences
+
+const collegeHealth = L.polygon([
+   [35.307945, -80.733588],
+   [35.307369, -80.733583],
+   [35.306973, -80.733645],
+   [35.306882, -80.733504],
+   [35.306858, -80.733245],
+   [35.306896, -80.733088],
+   [35.307067, -80.732849],
+   [35.307319, -80.733084],
+   [35.307853, -80.733126],
+   [35.307989, -80.733347],
+],{
+   color: 'blue',
+   fillColor: 'blue',
+   fillOpacity: 0.2,
+
+}).addTo(map)  
+
+//Burson
+
+const Burson = L.polygon([
+   [35.307775, -80.733069],
+   [35.307439, -80.733054],
+   [35.307082, -80.732389],
+   [35.307057, -80.732174],
+   [35.307057, -80.732174],
+   [35.307206, -80.731890],
+   [35.307752, -80.731923],
+],{
+   color: 'blue',
+   fillColor: 'blue',
+   fillOpacity: 0.2,
+
+}).addTo(map)  
+
+//Cameron
+
+const Cameron = L.polygon([
+   [35.307920438216186, -80.73160747819938],
+   [35.307432432774114, -80.73161170525938],
+   [35.30742504763413, -80.73080473815888],
+   [35.3079093452525, -80.73079870943644]
+   
+],{
+   color: 'blue',
+   fillColor: 'blue',
+   fillOpacity: 0.2,
+
+}).addTo(map)  
